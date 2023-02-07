df78df71f40ee49c1d03f2b3780dd43a32552c26
Karl <100312583+Karl-Espindola@users.noreply.github.com>
2023-02-07 00:01:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -47,9 +47,6 @@ let c_esta=document.getElementById("contenedor-estadisticas");
 let pikachu=document.getElementById("pikachu");
 
 document.addEventListener("keyup",function(e){
-    let l=document.getElementById("l");
-    l.innerText=e.code;
-    console.log(e.code);
     if(e.code=="Enter"){
         let busqueda=search.value;
         busqueda=busqueda.toLowerCase();
